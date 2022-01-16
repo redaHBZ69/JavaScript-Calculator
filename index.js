047fdf18c29e692215e644b23993a2b0074e02e5
Ahmed Reda Hbaiz <reda.hbaiiz@gmail.com>
2022-01-16 15:43:50 +0100

    Fixes

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
 /* jshint esversion: 11 */
-const input = require("prompt-sync")();
+const prompt = require("prompt-sync")({
+    sigint: true,
+    autocomplete: (str) => {
+        const commands = Object.getOwnPropertyNames(Math);
+        let ret = [];
+        for (const cmd of commands) {
+            if (cmd.indexOf(str) == 0)
+                ret.push(cmd);
+        }
+        return ret;
+    }
+});
 import("chalk").then((chalk) => {
-    console.log(`${new chalk.Chalk().redBright("=====================================")}
+    console.log(`${new chalk.Chalk().redBright("====================================================================================")}
 Welcome to the JavaScript calculator. Please use ${new chalk.Chalk().redBright("JavaScript Math")} syntax to calculate.
 \nNo need to put ${new chalk.Chalk().greenBright("Math.")} infront of Math functions/variables, that is handled automatically.
-${new chalk.Chalk().redBright("=====================================")}\n`);
+${new chalk.Chalk().redBright("====================================================================================")}\n`);
+
     let ans = 0;
 
     while (true) {
         try {
             let funcBody = `let ans=${ans};`;
+
             for (const identifier of Object.getOwnPropertyNames(Math)) {
                 funcBody += `let ${identifier}=Math.${identifier};`;
             }
-            const exp = input();
-            if (!exp) break;
+
+            const exp = prompt({ value: 0 });
+
             funcBody += `return ${exp};`;
+
             const result = new Function(funcBody);
+
             if (typeof result() !== "number") {
                 console.log("Could not compile. Please try again.\n");
                 continue;
             }
             console.log(result());
             ans = result();
+
         } catch (error) {
             console.log("An error occured. Please try again.");
         }
