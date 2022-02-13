2cfcd0237fb4a7296072826e5790f55dac0ff972
Ahmed Reda Hbaiz <reda.hbaiiz@gmail.com>
2022-02-13 18:26:16 +0100

    Added compare mode

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const prompt = require("prompt-sync")({
         }
         return ret;
     }
-});
+}),
+    { program } = require("commander");
+program.option("-c", "Run the script in compare mode where you can compare 2 values AND calculate");
+program.parse();
+const opts = program.opts();
 import("chalk").then((chalk) => {
     console.log(`${new chalk.Chalk().redBright("====================================================================================")}
 Welcome to the JavaScript calculator. Please use ${new chalk.Chalk().redBright("JavaScript Math")} syntax to calculate.
@@ -18,27 +22,32 @@ Welcome to the JavaScript calculator. Please use ${new chalk.Chalk().redBright("
 ${new chalk.Chalk().redBright("====================================================================================")}\n`);
 
     let ans = 0;
-
+    let bool = opts.c ? true : undefined;
     while (true) {
         try {
-            let funcBody = `let ans=${ans};`;
+            let funcBody = `const ans=${ans};const bool=${bool};`;
 
             for (const identifier of Object.getOwnPropertyNames(Math)) {
-                funcBody += `let ${identifier}=Math.${identifier};`;
+                funcBody += `const ${identifier}=Math.${identifier};`;
             }
 
             const exp = prompt({ value: 0 });
-
+            if(!opts.c && exp.includes("bool")) {
+                console.log("Cannot use 'bool' outside of compare mode. Add -c to args");
+                continue;
+            }
             funcBody += `return ${exp};`;
 
             const result = new Function(funcBody);
 
-            if (typeof result() !== "number") {
+            if (typeof result() !== "number" && (typeof result() !== "boolean" && !opts.c)) {
                 console.log("Could not compile. Please try again.\n");
                 continue;
             }
             console.log(result());
-            ans = result();
+            typeof result() == "boolean" ? bool = result() : ans = result();
+
+
 
         } catch (error) {
             console.log("An error occured. Please try again.");
